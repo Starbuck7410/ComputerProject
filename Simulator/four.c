44dab5f805960293653ad960d010fd2b3e3e5683
daniel <danielbrovender@gmail.com>
2024-12-15 16:43:21 +0200

    5

---
+++ Simulator/four.c
@@ -0,0 +1,4 @@
+
+int five() {
+	printf("4");
+}
--- Simulator/main.c
+++ Simulator/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int three();
-
+int five();
 int main() {
 	printf("I am a simulator bleep bloop: %d\n", three());
+	five();
 	return 0;
 }
