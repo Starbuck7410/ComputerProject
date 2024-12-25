6077154740bbb260d1a77ad7de571bbfb92b5760
Zohar <zohar.cre@gmail.com>
2024-12-25 15:16:43 +0200

    find_instruction function complete

--- Assembler/luts.c
+++ Assembler/luts.c
@@ -13,11 +13,31 @@ int eq_str(char str1[], char str2[]){
 }
 
 
-long long find_instruction(char name[]){
-    if (eq_str(name, "hello")){
-        return 0x50;
-    }
-    return 1;
+long long find_instruction(char name[]){ //This function recives a string of the instruction and returns its integer number.
+
+    if (eq_str(name, "add")) return 0;
+    if (eq_str(name, "sub")) return 1;
+    if (eq_str(name, "mac")) return 2;
+    if (eq_str(name, "and")) return 3;
+    if (eq_str(name, "or")) return 4;
+    if (eq_str(name, "xor")) return 5;
+    if (eq_str(name, "sll")) return 6;
+    if (eq_str(name, "sra")) return 7;
+    if (eq_str(name, "srl")) return 8;
+    if (eq_str(name, "beq")) return 9;
+    if (eq_str(name, "bne")) return 10;
+    if (eq_str(name, "blt")) return 11;
+    if (eq_str(name, "bgt")) return 12;
+    if (eq_str(name, "ble")) return 13;
+    if (eq_str(name, "bge")) return 14;
+    if (eq_str(name, "jal")) return 15;
+    if (eq_str(name, "lw")) return 16;
+    if (eq_str(name, "sw")) return 17;
+    if (eq_str(name, "reti")) return 18;
+    if (eq_str(name, "in")) return 19;
+    if (eq_str(name, "out")) return 20;
+    if (eq_str(name, "halt")) return 21;
+    
 }
 
 long long find_register(char reg[]) {     //This function recives a string starting with '$' and reg name and returns its integer number.
