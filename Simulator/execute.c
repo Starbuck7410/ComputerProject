c675a5ec7a1b61c06fd096172a5ebead80dd758c
Starbuck7410 <starbuck7410@gmail.com>
2025-01-12 17:20:57 +0200

    finished ALU

--- Simulator/execute.c
+++ Simulator/execute.c
@@ -11,7 +11,30 @@ int execute(int op_code, int * inst_regs, int * imms, int * registers){ // retur
     if(op_code == 0){ // ADD
         registers[inst_regs[0]] = registers[inst_regs[1]] + registers[inst_regs[2]] + registers[inst_regs[3]];
     }
-    
+    if(op_code == 1){ // SUB
+        registers[inst_regs[0]] = registers[inst_regs[1]] - registers[inst_regs[2]] - registers[inst_regs[3]];
+    }
+    if(op_code == 2){ // MAC
+        registers[inst_regs[0]] = registers[inst_regs[1]] * registers[inst_regs[2]] + registers[inst_regs[3]];
+    }
+    if(op_code == 3){ // AND
+        registers[inst_regs[0]] = registers[inst_regs[1]] & registers[inst_regs[2]] & registers[inst_regs[3]];
+    }
+    if(op_code == 4){ // OR
+        registers[inst_regs[0]] = registers[inst_regs[1]] | registers[inst_regs[2]] | registers[inst_regs[3]];
+    }    
+    if(op_code == 5){ // XOR
+        registers[inst_regs[0]] = registers[inst_regs[1]] ^ registers[inst_regs[2]] ^ registers[inst_regs[3]];
+    }
+    if(op_code == 6){ // SLL
+        registers[inst_regs[0]] = registers[inst_regs[1]] << registers[inst_regs[2]];
+    }
+    if(op_code == 7){ // SRA
+        registers[inst_regs[0]] = registers[inst_regs[1]] >> registers[inst_regs[2]];
+    }
+    if(op_code == 8){ // SRL
+        registers[inst_regs[0]] = (registers[inst_regs[1]] >> registers[inst_regs[2]]) & ~ (0xFFFFFFFF << registers[inst_regs[2]]);
+    }
     registers[0] = 0;
 
 }
